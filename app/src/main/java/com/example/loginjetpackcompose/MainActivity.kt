e1c66db9ef03730e3fda9035369671ef7004f549
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-27 14:39:27 +0200

    .

--- app/src/main/java/com/example/loginjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/loginjetpackcompose/MainActivity.kt
@@ -3,20 +3,28 @@ package com.example.loginjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.OptIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.loginjetpackcompose.ui.theme.LoginJetpackComposeTheme
@@ -61,15 +69,28 @@ fun Login(name: String, modifier: Modifier = Modifier) {
         }
         Column {
             Spacer(modifier = Modifier.height(53.dp))
-            TextField)()
+            TextField(value = "", label = "Password")
             Spacer(modifier = Modifier.height(15.dp))
-            Text(
+            TextField(
                 text = "Password:",
                 modifier = modifier
             )
         }
     }
 }
+
+@kotlin.OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleTextField() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text =
+        }
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
