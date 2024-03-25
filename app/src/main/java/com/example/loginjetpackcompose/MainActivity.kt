b7cd202beda6629c0a60cdbd42c59c5f7c91b01d
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-25 14:47:06 +0100

    .

--- app/src/main/java/com/example/loginjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/loginjetpackcompose/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.loginjetpackcompose
 
+import android.graphics.drawable.shapes.RoundRectShape
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
+import android.view.RoundedCorner
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,7 +33,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -64,36 +75,63 @@ fun Login(modifier: Modifier = Modifier) {
                 .size(300.dp, 100.dp)
         )
         Spacer(modifier = Modifier.height(25.dp))
-        Text(
-            text = "Username:",
-            modifier = modifier,
-            fontSize = 25.sp
-        )
-        Spacer(modifier = Modifier.height(7.dp))
-        TextField(
-            value = usernameTextField,
-            onValueChange = {newUsernameTextField -> usernameTextField = newUsernameTextField},
-            label = {Text(usernameTextField)},
-            modifier = modifier
-                .width(200.dp)
-                .height(25.dp)
-        )
-        Spacer(modifier = Modifier.height(15.dp))
-        Text(
-            text = "Password:",
-            modifier = modifier,
-            fontSize = 25.sp
-        )
-        Spacer(modifier = Modifier.height(7.dp))
-        TextField(
-            value = passwordTextField,
-            onValueChange = {newPasswordTextField -> passwordTextField = newPasswordTextField},
-            label = {Text(passwordTextField)},
-            modifier = modifier
-                .width(200.dp)
-                .height(25.dp)
-        )
+        Row (
+            modifier,
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Column (
+                modifier.padding(10.dp),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Username:",
+                    modifier = modifier,
+                    fontSize = 25.sp
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Password:",
+                    modifier = modifier,
+                    fontSize = 25.sp
+                )
+            }
+            Column (
+                modifier.padding(10.dp),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Center
+            ) {
+                TextField(
+                    value = usernameTextField,
+                    onValueChange = {newUsernameTextField -> usernameTextField = newUsernameTextField},
+                    label = {Text(usernameTextField)},
+                    modifier = modifier
+                        .width(200.dp)
+                        .height(25.dp)
+                )
+                Spacer(modifier = Modifier.height(40.dp))
+                TextField(
+                    value = passwordTextField,
+                    onValueChange = {newPasswordTextField -> passwordTextField = newPasswordTextField},
+                    label = {Text(passwordTextField)},
+                    modifier = modifier
+                        .width(200.dp)
+                        .height(25.dp)
+                )
+            }
+        }
         Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = "Login", fontSize = 20.sp)
+        }
     }
 }
 
